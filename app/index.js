f70c2023122c31ddc31230212827d945159f27a0
Ana Karen Hernandez Gonzalez <karen.hernandez@karen.hernandez>
2021-03-12 17:55:45 -0600

    github actions

--- app/index.js
+++ app/index.js
@@ -5,5 +5,5 @@ const port = process.env.PORT;
 const version = process.env.NODE_ENV.VERSION;
 if(version !== null) version = "local";
 
-app.get('/', (req, res) => res.send('Holiiii :) version: ' + version));
+app.get('/', (req, res) => res.send('Holiii :) version: ' + version));
 app.listen(port, () => console.log(port));
