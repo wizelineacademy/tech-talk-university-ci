ae5f99fc8c9032b20514b48f5633607750bbddd8
Luis Sandoval <luis.sandoval@wizeline.com>
2021-03-12 18:04:59 -0600

    Update version var on index.js

--- app/index.js
+++ app/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT;
 
-const version = process.env.NODE_ENV.VERSION;
+const version = process.env.VERSION;
 if(version !== null) version = "local";
 
 app.get('/', (req, res) => res.send('Holiss :) version: ' + version));
