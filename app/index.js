209ab83a22eafb92dd86fc9c1be25bcd00eddb7e
Luis Sandoval <luis.sandoval@wizeline.com>
2021-03-16 10:30:24 -0600

    Add index sample

--- app/index.js
+++ app/index.js
@@ -1,10 +1,14 @@
 const express = require('express');
+const path = require('path');
 const app = express();
 const port = process.env.PORT;
 const version = process.env.VERSION;
+const templates = path.resolve(__dirname, 'templates');
+const index = path.resolve(templates, 'index.html');
 
 app.get('/', (req, res) => {
-    res.send('Holiss :) version: ' + version)
+    //res.send('Holiss :) version: ' + version)
+    res.sendFile(index);
 });
 
 app.get('/health', (req, res) => {
