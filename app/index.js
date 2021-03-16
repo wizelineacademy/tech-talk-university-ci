f6720d466d6d6c56277c1f435523f62107848d11
Luis Sandoval <luis.sandoval@wizeline.com>
2021-03-16 10:11:42 -0600

    Add health endpoint and heroku health checker

--- app/index.js
+++ app/index.js
@@ -7,4 +7,6 @@ const version = process.env.VERSION;
 console.log(process.env);
 
 app.get('/', (req, res) => res.send('Holiss :) version: ' + version));
+app.get('/health', (req, res) => res.send('OK'));
+
 app.listen(port, () => console.log(port));
