7a62f9e0ff529690e48a9eeeabdd4f599d5deeab
Luis Sandoval <luis.sandoval@wizeline.com>
2021-03-16 10:39:36 -0600

    Update package.json and docker file adding npm start

--- app/index.js
+++ app/index.js
@@ -3,7 +3,7 @@ const path = require('path');
 const app = express();
 const port = process.env.PORT;
 const version = process.env.VERSION;
-const templates = path.resolve(__dirname, './app/templates');
+const templates = path.resolve(__dirname, 'templates');
 const index = path.resolve(templates, 'index.html');
 
 app.get('/', (req, res) => {
