e41ac762d344d6c25b6ca575909069bfad9b0fb2
Luis Sandoval <luis.sandoval@wizeline.com>
2021-03-16 10:19:03 -0600

    Update index.js with health api

--- app/index.js
+++ app/index.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const app = express();
 const port = process.env.PORT;
-
 const version = process.env.VERSION;
 
-console.log(process.env);
+app.get('/', (req, res) => {
+    res.send('Holiss :) version: ' + version)
+});
 
-app.get('/', (req, res) => res.send('Holiss :) version: ' + version));
-app.get('/health', (req, res) => res.send('OK'));
+app.get('/health', (req, res) => {
+    res.send('OK')
+});
 
 app.listen(port, () => console.log(port));
